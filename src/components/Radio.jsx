9808a5e539be30b1b525a245b98b1ec19b0f72a9
jsk900 <jsk900@yahoo.co.uk>
2021-12-01 11:17:49 +0100

    inputs

---
+++ src/components/Radio.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const Radio = () => {
+  const [radio, setRadio] = useState('Berlin');
+
+  return (
+    <div>
+      <input
+        type='radio'
+        value='Berlin'
+        checked={radio === 'Berlin'}
+        onChange={(e) => setRadio(e.target.value)}
+      />
+      <input
+        type='radio'
+        value='London'
+        checked={radio === 'London'}
+        onChange={(e) => setRadio(e.target.value)}
+      />
+      <input
+        type='radio'
+        value='Madrid'
+        checked={radio === 'Madrid'}
+        onChange={(e) => setRadio(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Radio;
